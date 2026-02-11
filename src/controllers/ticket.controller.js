800d92a30d88e0652277518bd10dc5d7598c250a
uniqueusha <ushamyadav777@gmail.com>
2026-02-11 15:22:36 +0530

    changes

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -870,8 +870,7 @@ const getMonthWiseStatusCount = async (req, res) => {
         FROM tickets t
         LEFT JOIN ticket_assignments ta ON ta.ticket_id = t.ticket_id
         LEFT JOIN customers c ON c.user_id = t.user_id
-        LEFT JOIN customer_agents ca ON ca.customer_id = t.customer_id
-
+        
         WHERE DATE(t.created_at) BETWEEN ? AND ?`;
 
         // if (user_id) {
@@ -881,7 +880,7 @@ const getMonthWiseStatusCount = async (req, res) => {
         if (user_id) {
             statusCountQuery += `
                 AND ( t.user_id = ${user_id} OR ta.assigned_to = ${user_id}
-                    OR (ta.assigned_to IS NULL AND ca.user_id = ${user_id})
+                    OR (ta.assigned_to IS NULL)
                 )
             `;
         }
