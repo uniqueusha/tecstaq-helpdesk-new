a73b467fa4fee6e4af70e140b1ed09779cfa4745
uniqueusha <ushamyadav777@gmail.com>
2026-02-14 12:30:54 +0530

    chnages

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -973,8 +973,8 @@ const getAllTickets = async (req, res) => {
         //     countQuery += ` AND ((ta.assigned_to IS NULL AND ca.user_id = ${user_id}) OR ta.assigned_to = ${user_id} OR t.user_id = ${user_id}) OR t.ticket_status = 'Re-assign'`;
         // }
         if (user_id) {
-            getTicketsQuery += ` AND ((ta.assigned_to IS NULL AND ca.user_id = ${user_id}) OR ta.assigned_to = ${user_id} OR (t.user_id = ${user_id} AND t.customer_user_id = ${user_id}) OR t.ticket_status = 'Re-assign' )`;
-            countQuery += ` AND ((ta.assigned_to IS NULL  AND ca.user_id = ${user_id}) OR ta.assigned_to = ${user_id} OR (t.user_id = ${user_id} AND t.customer_user_id = ${user_id})  OR t.ticket_status = 'Re-assign')`;
+            getTicketsQuery += ` AND ((ta.assigned_to IS NULL AND ca.user_id = ${user_id}) OR ta.assigned_to = ${user_id} OR (t.user_id = ${user_id} AND t.customer_user_id = ${user_id}) OR ts.changed_by = ${user_id} OR t.ticket_status = 'Re-assign' )`;
+            countQuery += ` AND ((ta.assigned_to IS NULL  AND ca.user_id = ${user_id}) OR ta.assigned_to = ${user_id} OR (t.user_id = ${user_id} AND t.customer_user_id = ${user_id}) OR ts.changed_by = ${user_id} OR t.ticket_status = 'Re-assign')`;
         }
 
         if (assigned_to) {
